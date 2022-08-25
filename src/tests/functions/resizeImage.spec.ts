d6649858607961dcc822f655a0b7df81a7dd296f
Ali Kehel <ali.saber.27.2@gmail.com>
2022-08-25 17:37:36 +0200

    add vey basic jasmine testing

--- src/tests/functions/resizeImage.spec.ts
+++ src/tests/functions/resizeImage.spec.ts
@@ -1,7 +1,14 @@
 import resizeImage from '../../functions/resizeImage';
-
+import fs from 'fs/promises';
 describe('test resizing images', () => {
-	it('should be resolved', async () => {
+	it('resizing image should be resolved', async () => {
 		await expectAsync(resizeImage('image1', 100, 100)).toBeResolved();
+	});
+
+	it('resizing image should work', async () => {
+		await fs.rm('./assets/thumbnails/image1-100-100.jpg');
+		await resizeImage('image1', 100, 100);
+		// const image = await fs.open('../../../assets/thumbnails/image1-100-100.jpg');
+		await expectAsync(fs.open('./assets/thumbnails/image1-100-100.jpg')).toBeResolved();
 	});
 });
